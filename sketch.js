c8be2bda8a8f6453c13e043e05d33b46111fdcaf
nateprieto <93701883+nateprieto@users.noreply.github.com>
2021-11-08 13:46:46 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,15 +3,20 @@ let palette1 = ['#3c1518', '#69140e', '#a44200', '#d58936', '#f2f3ae'];
 let palette2 = ['#313238', '#4e4232', '#a08357', '#f1c47b', '#e8ddcb'];
 let palette3 = ['#1c2321', '#5e6572', '#7d98a1', '#a9b4c2', '#eef1ef'];
 let array2 = ['Sol   ', '   ita   ', '      ire', 'Solita   ', '   itaire', 'Sol   ire'];
+let array3 = ['S', 'o', 'l', 'i', 't','a','i', 'r', 'e'];
+let freqArray = [5, 10, 10, 10, 5, 15, 20, 26];
+let countArray = [2, 3, 3, 4, 4, 4, 5, 5];
 let currentPalette = palette1;
 let currentDesign = 1;
 let palette = 1;
+let clicked = 0;
 function setup() {
   createCanvas(400, 640);
   font = loadFont('Lunchtype23-Medium-Italic.ttf');
 }
 
 function draw() {
+  randomSeed(60);
   background(currentPalette[0]);
   textFont(font);
   textSize(70);
@@ -29,13 +34,14 @@ function draw() {
   
   if(currentDesign % 3 == 1){
     designOne();
+    
   }
   else if(currentDesign % 3 == 2){
     designTwo();
   }
   else if(currentDesign % 3 == 0){
     designThree();
-  }  
+  }
 }
 
 function keyPressed(){
@@ -47,25 +53,50 @@ function keyPressed(){
   }
 }
 
+function mouseReleased(){
+  if(clicked === 0){
+    clicked = 1;
+    if(currentDesign % 3 == 1){
+    designOne();
+    }
+  }
+  else{
+    clicked = 0;
+  }
+}
 
 function designOne(){
   noStroke();
+  let amp = 20;
+  if(clicked){
+    amp = 50 * ((mouseY/height));
+  }
+  else if(clicked == 0){
+    freqArray[0] = 5;
+    freqArray[1] = 10;
+    freqArray[2] = 10;
+    freqArray[3] = 10;
+    freqArray[4] = 5;
+    freqArray[5] = 15;
+    freqArray[6] = 20;
+    freqArray[7] = 26;
+  }
   let char = 'o';
-  textAnimate(char, 140, 320, 5);
+  textAnimate(char, 140, 320, freqArray[0], countArray[0], amp);
   char = 'l';
-  textAnimate(char, 165, 310, 10);
+  textAnimate(char, 165, 310, freqArray[1], countArray[1], amp);
   char = 'i';
-  textAnimate(char, 180, 325, 10);
+  textAnimate(char, 180, 325, freqArray[2], countArray[2], amp);
   char = 't';
-  textAnimate(char, 200, 320, 10);
+  textAnimate(char, 200, 320, freqArray[3], countArray[3], amp);
   char = 'a';
-  textAnimate(char, 230, 310, 5);
+  textAnimate(char, 230, 310, freqArray[4], countArray[4], amp);
   char = 'i';
-  textAnimate(char, 260, 315, 15);
+  textAnimate(char, 260, 315, freqArray[5], countArray[5], amp);
   char = 'r';
-  textAnimate(char, 280, 310, 20);
+  textAnimate(char, 280, 310, freqArray[6], countArray[6], amp);
   char = 'e';
-  textAnimate(char, 310, 315, 26);
+  textAnimate(char, 310, 315, freqArray[7], countArray[7], amp);
   fill(currentPalette[3]);
   text('Solitaire', 200, 320);
   fill(currentPalette[1]);
@@ -73,21 +104,7 @@ function designOne(){
   text('By Microsoft', 325, 620);
 }
 
-function textAnimate(char, xOff, yOff, freq){
-  let count = 2;
-  if(xOff >= 280){
-    count = 5;
-  }
-  else if(xOff <= 140){
-    count = 2;
-  }
-  else if(xOff >= 200){
-    count = 4;
-  }
-  else if(xOff < 200){
-    count = 3;
-  }
-  
+function textAnimate(char, xOff, yOff, freq, count, amp){
   for(let i = 1; i < count; i ++){
     if(i == 1 && xOff < 200){
       fill(currentPalette[1]);
@@ -107,13 +124,17 @@ function textAnimate(char, xOff, yOff, freq){
     else if(i == 4){
       fill(currentPalette[1]);
     }
-    let n = yOff + freq*sin(i * freq) + freq * noise(-i/10 + millis()/1000);
+    let n = yOff + freq*sin(i * freq) + amp * noise(-i/10 + millis()/1000);
     text(char, xOff, n);
   }
 }
 
 function designTwo(){
   noStroke();
+  let amp = 15;
+  if(clicked){
+    amp = 60 * ((mouseY/height));
+  }
   let xOff = 180;
   let bound = 690;
   for(let i = 1; i < 7; i ++){
@@ -122,7 +143,7 @@ function designTwo(){
       index += 1;
     }
     fill(currentPalette[index]);
-    let n = 320 + 15*sin(i * 15) + 15 * noise(-i/10 + millis()/1000);
+    let n = 320 + 15*sin(i * 15) + amp * noise(-i/10 + millis()/1000);
     text(array2[i % 4], xOff, bound - n);
     xOff -= 30;
     bound += 50;
@@ -135,8 +156,8 @@ function designTwo(){
     if(index == 0 || index == 3){
       index += 1;
     }
-    fill(currentPalette[index])
-    let n = 320 + 15*sin(i * 15) + 15 * noise(-i/10 + millis()/1000);
+    fill(currentPalette[index]);
+    let n = 320 + 15*sin(i * 15) + amp * noise(-i/10 + millis()/1000);
     text(array2[i % 5], xOff, n - bound);
     xOff += 30;
     bound -= 50;
@@ -152,7 +173,6 @@ function designTwo(){
 
 function designThree(){
   noStroke();
-  let array3 = ['S', 'o', 'l', 'i', 't', 'a', 'i', 'r', 'e'];
   fill(currentPalette[2]);
   text(array3[0], 95, 320);
   fill(currentPalette[3]);
